2797585b7d5a20680d4d967b36531b89ae5a0c38
mdai <maya.dai@eteration.com>
2026-06-19 14:52:07 +0000

    Fix RViz node in launch file to find correct config file path

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -6,7 +6,7 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    # rviz_config_dir = os.path.join(get_package_share_directory('robot_patrol'), 'src', 'robot_patrol_config.rviz')
+    rviz_config_dir = '/home/user/ros2_ws/src/citylab_project/robot_patrol/src/robot_patrol_config.rviz'
     
     # Nodes
     patrol_node = launch_ros.actions.Node(
@@ -20,8 +20,8 @@ def generate_launch_description():
         package='rviz2',
         executable='rviz2',
         name='rviz2',
-        # arguments=['-d', rviz_config_dir],
-        arguments=[],
+        arguments=['-d', rviz_config_dir],
+        # arguments=[],
         output='screen',
     )
 
