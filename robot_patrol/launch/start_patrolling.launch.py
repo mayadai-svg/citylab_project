8aec3b0f8c7699b863a98eb03c6477de5ca9a21d
mdai <maya.dai@eteration.com>
2026-06-19 16:54:56 +0000

    Update real_robot launch file to include RViz launch

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -3,12 +3,22 @@ import launch_ros.actions
 
 
 def generate_launch_description():
-    
+
+    rviz_config_dir = '/home/user/ros2_ws/src/citylab_project/robot_patrol/src/real_robot_config.rviz'
+
     # Nodes
     patrol_node = launch_ros.actions.Node(
         package='robot_patrol',
         executable='patrol',
         arguments=[],
+        output='screen',
+    )
+
+    rviz_node = launch_ros.actions.Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_dir],
         output='screen',
     )
 
