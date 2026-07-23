f8b5ebf4e1e1166cba7eaa10177d4c301c619469
mdai <maya.dai@eteration.com>
2026-07-23 20:08:15 +0000

    Add patrol_with_service executable & fix main launch file to launch patrol_with_service.cpp

--- robot_patrol/launch/main.launch.py
+++ robot_patrol/launch/main.launch.py
@@ -5,24 +5,36 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description(): 
-   
+    
+    rviz_config_dir = '/home/user/ros2_ws/src/citylab_project/robot_patrol/src/robot_patrol_config.rviz'
+
     # Nodes
     service_node = launch_ros.actions.Node(
         package='robot_patrol',
         executable='direction_service',
         name= 'direction_service',
         arguments=[],
-        output='screen',
+        output='screen'
     )
 
     patrol_node = launch_ros.actions.Node(
         package='robot_patrol',
-        executable='patrol',
+        executable='patrol_with_service',
         arguments=[],
+        output='screen'
+    )
+
+    rviz_node = launch_ros.actions.Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_dir],
+        # arguments=[],
         output='screen',
     )
 
     return launch.LaunchDescription([
        service_node,
-       patrol_node
+       patrol_node,
+       rviz_node
     ])
