9ed5910caa0486f5c33992aecbdec98a87b3539c
mdai <maya.dai@eteration.com>
2026-07-27 20:00:05 +0000

    Add rviz to gotopose launch file

--- robot_patrol/launch/start_gotopose_action.launch.py
+++ robot_patrol/launch/start_gotopose_action.launch.py
@@ -4,7 +4,10 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 
-def generate_launch_description():    
+def generate_launch_description(): 
+
+    rviz_config_dir = '/home/user/ros2_ws/src/citylab_project/robot_patrol/src/robot_patrol_config.rviz'
+
     # Nodes
     action_node = launch_ros.actions.Node(
         package='robot_patrol',
@@ -14,6 +17,16 @@ def generate_launch_description():
         output='screen',
     )
 
+    rviz_node = launch_ros.actions.Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_dir],
+        # arguments=[],
+        output='screen',
+    )
+
     return launch.LaunchDescription([
-       action_node
+       action_node,
+       rviz_node
     ])
